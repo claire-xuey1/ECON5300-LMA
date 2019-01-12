cbe6a92a1b9ea0b211e0dee33f50f803b262c948
jpflugrath <jillianpflugrath@gmail.com>
2019-01-12 15:02:33 -0800

    TEST - Initial Commit

---
+++ EDA - Due Jan 23.R
@@ -0,0 +1,24 @@
+EDA, Sample Selection Criteria, Descriptive Statistics | Due January 23
+
+Using Excel, limit the data file to variables of potential interest.  Using R Studio, perform exploratory data analysis (EDA).  Based on your EDA, determine appropriate sample selection criteria.  Apply these selection criteria and generate descriptive statistics. Refer to the projects module in Canvas for resources and materials.
+
+For the EDA:
+  
+  examine the first 10 or 20 observations (rows of data) corresponding to variables of interest (columns) and compare the observed values to the data dictionaryLinks to an external site.for person records
+compute and examine descriptive statistics including the minimum, maximum, mean, and median for quantitative variables of interest
+generate and examine histograms for quantitative variables of interest
+generate and examine bar charts/graphs for qualitative variables of interest
+generate and examine cross tabulations, scatterplots, and/or correlation coefficients of interest
+What did you learn about the data based on the EDA?  With the findings and insights from the EDA, think carefully about sample selection criteria.  For example, do you want to restrict your sample to individuals who are employed or those who are employed full-time?  Do you want to restrict your sample to those with positive earnings – or with earnings above some realistic threshold? 
+  
+  For examples of sample selection criteria, see the two sample labor market analyses in the projects module.  In particular, see the first paragraph of the data section in Benito, Glassman, and Hiedemann and section 3.1 of Choi, Joesch, and Lundberg.
+
+Using R Studio, apply these sample selection criteria.  Then compute and present relevant descriptive statistics for the full sample and/or by subsample, as appropriate.  For example, based on the second pair of research questions above, present descriptive statistics for men and women separately. 
+
+On Canvas, report:
+  
+  your research questions and geographic area for context
+a brief discussion of insights obtained from the EDA
+a list of sample selection criteria
+a rationale for each sample selection criteria
+descriptive statistics (after imposing sample selection criteria)
